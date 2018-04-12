af1fc63a2022853c84c487a08c7c5c62c49ae92d
Edward Seabrook <edderick@live.co.uk>
2018-04-12 03:01:24 +0100

    Not async

--- async_email_sender.py
+++ async_email_sender.py
@@ -1,6 +1,6 @@
 
 import smtplib
-from multiprocessing import Pool # Here are the big guns
+#from multiprocessing import Pool # Here are the big guns
 
 def get_password():
     with open('./password.txt') as f:
@@ -10,7 +10,7 @@ def callback(arg):
     # TODO: Track failed messages?
     print arg
 
-def send_email_job(to, subject, body):
+def send_email(to, subject, body):
     gmail_user = 'bethan.and.edward.wedding@gmail.com'
     gmail_password = get_password()
 
@@ -27,11 +27,12 @@ def send_email_job(to, subject, body):
     server.close()
     print "Email sent: {} - {} - {}".format(to, subject, body)
 
-email_thread_pool = Pool(processes=1)
+#email_thread_pool = Pool(processes=1)
 
-def send_email(to, subject, body):
-    print "Sending email to {} - {} - {}".format(to, subject, body)
-    e = (to, subject, body)
-    email_thread_pool.apply_async(send_email_job, e, callback=callback)
+# TODO: Figure out how to make it async and work
+#def send_email(to, subject, body):
+#    print "Sending email to {} - {} - {}".format(to, subject, body)
+#    e = (to, subject, body)
+#    email_thread_pool.apply_async(send_email_job, e, callback=callback)
 
 
